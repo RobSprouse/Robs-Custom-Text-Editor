1ec81ac1e5fe2acb2c708d959981a4c9e2a70ffa
RobSprouse <141681347+RobSprouse@users.noreply.github.com>
2024-01-16 12:07:19 -0500

    Add copy-webpack-plugin and mini-css-extract-plugin dependencies and configure webpack

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,7 +17,8 @@ export const putDb = async (content) => {
      const jateDb = await openDB("jate", 1);
      const tx = jateDb.transaction("jate", "readwrite");
      const store = tx.objectStore("jate");
-     const request = store.add({ jateContent: content });
+     await store.clear();
+     const request = store.add({ content: content });
      const result = await request;
      console.log("Data saved to the database", result);
 };
@@ -30,7 +31,7 @@ export const getDb = async () => {
      const request = store.getAll();
      const result = await request;
      console.log("result.value", result);
-     return result;
+     return result[0]?.content;
 };
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,30 +1,83 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const copyWebpackPlugin = require("copy-webpack-plugin");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
-  return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      
-    ],
+     return {
+          mode: "production",
+          entry: {
+               main: "./src/js/index.js",
+               install: "./src/js/install.js",
+          },
+          output: {
+               filename: "[name].bundle.js",
+               path: path.resolve(__dirname, "dist"),
+          },
+          plugins: [
+               new HtmlWebpackPlugin({
+                    template: "./index.html",
+                    title: "Robs Custom Text Editor",
+               }),
+               new InjectManifest({
+                    swSrc: "./src-sw.js",
+               }),
+               new MiniCssExtractPlugin({
+                    filename: "assets/css/[name].css",
+               }),
+               new WebpackPwaManifest({
+                    name: "Robs Custom Text Editor",
+                    inject: true,
+                    fingerprints: false,
+                    display: "standalone",
+                    short_name: "Robs TE",
+                    description: "A simple browser text editor.",
+                    start_url: "/",
+                    publicPath: "/",
+                    icons: [
+                         {
+                              src: path.resolve(__dirname, "./src/images/logo.png"),
+                              sizes: [96, 128, 192, 256, 384, 512],
+                              destination: "assets/icons",
+                         },
+                    ],
+               }),
+               new copyWebpackPlugin({
+                    patterns: [
+                         {
+                              from: "./favicon.ico",
+                              to: "./favicon.ico",
+                         },
+                    ],
+               }),
+          ],
 
-    module: {
-      rules: [
-        
-      ],
-    },
-  };
+          module: {
+               rules: [
+                    {
+                         test: /\.css$/i,
+                         use: [MiniCssExtractPlugin.loader, "css-loader"],
+                    },
+                    {
+                         test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                         type: "asset/resource",
+                    },
+                    {
+                         test: /\.m?js$/,
+                         exclude: /(node_modules|bower_components)/,
+                         use: {
+                              loader: "babel-loader",
+                              options: {
+                                   presets: ["@babel/preset-env"],
+                              },
+                         },
+                    },
+               ],
+          },
+     };
 };
